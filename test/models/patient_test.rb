39da35772782f1b83ed1cb2b8af5a5707289ee83
cast3 <a.castellanos.lizcano@gmail.com>
2023-11-21 04:44:09 +0000

    formateo de prueba

--- test/models/patient_test.rb
+++ test/models/patient_test.rb
@@ -14,13 +14,13 @@ class PatientTest < ActiveSupport::TestCase
     result = patient.calcularMasaMaximaNormal
     expected_result = 25 * (1.75 * 1.75)
     assert_in_delta expected_result, result
- end
+  end
 
   test "calcularCategoriaPresionArterial calculates blood pressure category correctly" do
     patient = Patient.new(systolic_pressure: 120, diastolic_pressure: 80)
     result = patient.calcularCategoriaPresionArterial
     assert_equal "Hipertensión", result
- end
+  end
 
   test "calcularCategoriaMasaCorporal calculates BMI category correctly" do
     patient = Patient.new(height: 1.75, mass: 105)
